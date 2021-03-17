2fb2e958437be953d4254eb654662a3fb2456b4c
patrikduch <duchpatrik@icloud.com>
2021-03-17 06:05:03 +0100

    Consumer: Basic functionality

--- Consumer/Receiver.cs
+++ Consumer/Receiver.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
 
 namespace Consumer
 {
@@ -6,7 +9,31 @@ namespace Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var conn = factory.CreateConnection())
+            using (var channel = conn.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+
+                consumer.Received += (model, ea) =>
+                {
+
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine("Received message {0} ...", message);
+
+                };
+
+                channel.BasicConsume("BasicTest", true, consumer);
+                Console.WriteLine("Press [Enter] to exit the Consumer App..");
+            };
         }
     }
 }
